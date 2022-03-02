2648162f5fb2c8d7ef69b709e2fc48adb4333c84
virsavik <dhuuloc8818@gmail.com>
2022-03-02 17:11:57 +0700

    Remove những thư viện không cần thiết

--- src/pages/TrainingPage/TrainingPage.tsx
+++ src/pages/TrainingPage/TrainingPage.tsx
@@ -1,9 +1,5 @@
-import { Box, Divider, LinearProgress, Paper } from '@mui/material';
-import React, { useEffect } from 'react';
-import { TextField, Grid, Button } from '@mui/material';
-import { useAppDispatch, useAppSelector } from 'src/configs/store';
-import { getAllCategories, findCategoryByName } from './category.reducer';
-import { Outlet, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const Training = () => {
   return <Outlet />;
